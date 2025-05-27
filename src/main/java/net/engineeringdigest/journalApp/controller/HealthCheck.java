ac0775612138f1edec69350ec2b1eaf0861b1ed9
adityaprajapati10 <adityababuprajapti@gmail.com>
2025-05-27 17:39:50 +0530

    "Temp. update in helth chck"

--- src/main/java/net/engineeringdigest/journalApp/controller/HealthCheck.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/HealthCheck.java
@@ -8,6 +8,6 @@ public class HealthCheck {
 
     @GetMapping("/health-check")
     public String healthCheck(){
-        return "ok health check done";
+        return "ok health check don";
     }
 }
