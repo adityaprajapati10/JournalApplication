c7a7d038931138d3e2b76925a4116cdabd4436c6
adityaprajapati10 <adityababuprajapati@gmail.com>
2025-05-29 17:55:54 +0530

    Project Lombook implement in the project

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,6 +1,7 @@
 package net.engineeringdigest.journalApp.entity;
 
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,48 +11,12 @@ import java.util.Date;
 import java.util.Objects;
 
 @Document(collection = "journal_entries") // the entity is mapped with mongodb collection
+@Data
 public class JournalEntry {
 
     @Id
     private ObjectId id;
-
     private  String title;
-
     private String content;
-
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-
 }
