ddd046d9a863f143c2d18cde9d62946ec0a2395a
adityaprajapati10 <adityababuprajapati@gmail.com>
2025-06-05 10:45:22 +0530

    implemented Transactional annotation

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -3,10 +3,16 @@ package net.engineeringdigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 // controller ---> service ---> repository(interface)
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalApplication {
 
     public static void main(String[] args) {
@@ -14,5 +20,12 @@ public class JournalApplication {
         SpringApplication.run(JournalApplication.class, args);
     }
 
+    // PlatformTransactionManager(interface)
+    // MongoTransactionManager(implementation of PlatformTransactionManager)
+    // Use this MongoTransactionManager as the transaction manager for my application, and wire it up using the provided MongoDatabaseFactory.
+    @Bean
+    public PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+    }
 
 }
