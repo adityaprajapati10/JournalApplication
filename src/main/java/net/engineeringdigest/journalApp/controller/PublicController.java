e49cb0c57d45acfb548f5d0bee0a430902dc17ca
adityaprajapati10 <adityababuprajapati@gmail.com>
2025-06-12 12:04:46 +0530

    Role Based Authorization implemented Admin role

--- src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -19,7 +19,7 @@ public class PublicController {
 
     @PostMapping("/create-user")
     public void createUser(@RequestBody User user){
-        userService.saveNewEntry(user);
+        userService.saveNewUser(user);
     }
 
 }
